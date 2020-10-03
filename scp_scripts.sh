cbe47cf5f09d6a71df8b4e8c4c99e9764e56680e
legelf36 <krfleg2@accesscomm.ca>
2020-10-03 15:03:59 -0600

    update to scripts

--- scp_scripts.sh
+++ scp_scripts.sh
@@ -1,1 +1,7 @@
+# Syntax for "scp"
+#
+# copy file to host
 # scp SourceFile user@host:directory/TargetFile
+#
+# copy file from host and copying folder from host
+# scp -r user@host:directory/SourceFolder TargetFolder
