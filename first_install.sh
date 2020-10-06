62ddb30bd636c39574ccb34e6d4c78cf4b3e73d1
legelf36 <krfleg2@accesscomm.ca>
2020-10-05 21:02:35 -0600

    update to scripts

--- first_install.sh
+++ first_install.sh
@@ -2,11 +2,11 @@
 #!/bin/bash
 
 # Call 'apt-update.sh' to run updates
-$HOME/.scripts/apt-update.sh
+./apt-update.sh
 
 # Call '100-install-desk-first.sh' to install basic software
-$HOME/.scripts/i3-gaps-install/100-install-desk-first.sh
+./i3-gaps-install/100-install-desk-first.sh
 
 # Call '140-install-extra-software.sh' to install extra software
-$HOME/.scripts/i3-gaps-install/140-install-extra-software.sh
+./i3-gaps-install/140-install-extra-software.sh
 
