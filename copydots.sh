c7ea808f113c982e001cec00da75e7092889e2ce
legelf36 <krfleg2@accesscomm.ca>
2020-10-27 20:29:35 -0600

    update to scripts

--- copydots.sh
+++ copydots.sh
@@ -3,6 +3,6 @@ cp -f ~/.bashrc ~/projects/desk-LMDE4-dotfiles/
 cp -f ~/.bash_aliases ~/projects/desk-LMDE4-dotfiles/
 cp -f ~/.profile ~/projects/desk-LMDE4-dotfiles/
 cp -f ~/.vimrc ~/projects/desk-LMDE4-dotfiles/
-#cp -rf ~/.config/i3/ ~/projects/desk-LMDE4-dotfiles/.config/
-#cp -rf ~/.config/powerline-shell/ ~/projects/desk-LMDE4-dotfiles/.config/
+cp -f ~/.config/ranger/rc.conf ~/projects/desk-LMDE4-dotfiles/.config/
+
 
